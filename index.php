b3e83fd6a6c3ff9e548ba8284e5740174f9d1ba3
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-13 15:21:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,9 @@
 </head>
 <body>
 <strong><a href=https://github.com/SK-2019/php-sql-wprowadzenie-Olek-Zawadzki>Github</a></strong>
+<div class="nav">
+    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+    <a class="nav_link" href="data_czas.php">Data i Czas</a>
+</div>
