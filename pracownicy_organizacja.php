4affaa3dd182782c4f673552d0884f57b4defb20
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-13 18:11:51 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -152,3 +152,23 @@
                     }
                 echo("</table>");
                 echo("<hr />");
+    
+                echo("<h3>Kobiety z działu 1 i 3 posortowane rosnąco po zarobkach</h3>");
+        $sql=('SELECT * FROM pracownicy,organizacja where imie like "%a" and dzial=id_org and (dzial=1 or dzial=3) group by nazwa_dzial order by zarobki asc');
+            $result=$conn->query($sql);//mysql
+                echo("<table border=1>");
+                echo("<li>SQL: $sql");
+                echo("<th>id</th>");
+                echo("<th>imie</th>");
+                echo("<th>dzial</th>");
+                echo("<th>zarobki</th>");
+                echo("<th>nazwa_dzial</th>");
+                echo("<th>data_urodzenia</th>");
+                    while($row=$result->fetch_assoc()){
+                        echo("<tr>");
+                            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                        echo("</tr>");
+                    }
+                echo("</table>");
+                echo("<hr />");
+    
