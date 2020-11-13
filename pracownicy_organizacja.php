025992599c4f6da45a7aeab655aa8d996600c47f
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-13 17:40:37 +0100

    Create pracownicy_organizacja.php

---
+++ pracownicy_organizacja.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<strong><a href=https://github.com/SK-2019/php-sql-wprowadzenie-Olek-Zawadzki>Github</a></strong>
+<div class="nav">
+    <a class="nav_link" href="index.php">Strona Startowa</a>
+    <a class="nav_link" href="pracownicy.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="funkcjeagregujace.php">Pracownicy i Organizacja</a>
+    <a class="nav_link" href="data_czas.php">Data i Czas</a>
+</div>
+<h1>Olek Zawadzki 2ti 31</h1>
+<h2>Pracownicy i Organizacja</h2> 
