9baf913f2dd88a68e497e19f45f2c991c6db0ace
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-13 16:11:18 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -18,7 +18,7 @@
             echo("<h3>Pracownicy tylko z działu 2</h3>");
             require_once('conn.php');
     $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=2 group by nazwa_dzial');
-        $result=$conn->query($sql);//mysql
+        $result=$conn->query($sql);
             echo("<table border=1>");
             echo("<li>SQL: $sql");
             echo("<th>id</th>");
@@ -34,4 +34,23 @@
                 }
             echo("</table>");
             echo("<hr />");
+    
+     echo("<h3>Pracownicy tylko z działu 2 i z działu 3</h3>");
+        $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=2 or dzial=3 group by nazwa_dzial');
+            $result=$conn->query($sql);
+                echo("<table border=1>");
+                echo("<li>SQL: $sql");
+                echo("<th>id</th>");
+                echo("<th>imie</th>");
+                echo("<th>dzial</th>");
+                echo("<th>zarobki</th>");
+                echo("<th>nazwa_dzial</th>");
+                echo("<th>data_urodzenia</th>");
+                    while($row=$result->fetch_assoc()){
+                        echo("<tr>");
+                            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                        echo("</tr>");
+                    }
+                echo("</table>");
+                echo("<hr />");
     ?>
