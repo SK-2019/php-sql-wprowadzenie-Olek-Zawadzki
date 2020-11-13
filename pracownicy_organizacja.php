1c661bb18df01eb29fe7c849e4d86d51e2552019
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-13 17:59:01 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -85,3 +85,10 @@
                 echo("<th>zarobki</th>");
                 echo("<th>nazwa_dzial</th>");
                 echo("<th>data_urodzenia</th>");
+                    while($row=$result->fetch_assoc()){
+                        echo("<tr>");
+                            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                        echo("</tr>");
+                    }
+                echo("</table>");
+                echo("<hr />");
