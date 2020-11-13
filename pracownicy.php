c9b0fee4eb6635feefd74595a1cea993ac7b7648
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-13 16:04:44 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -12,5 +12,26 @@
     <a class="nav_link" href="data_czas.php">Data i Czas</a>
 </div>
 <h1>Olek Zawadzki 2ti 31</h1>
-    <?php
-   require_once('conn.php');
+<h2>Tabela Pracowników</h2>
+<?php
+            echo("<hr />");
+            echo("<h3>Pracownicy tylko z działu 2</h3>");
+            require_once('conn.php');
+    $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=2 group by nazwa_dzial');
+        $result=$conn->query($sql);//mysql
+            echo("<table border=1>");
+            echo("<li>SQL: $sql");
+            echo("<th>id</th>");
+            echo("<th>imie</th>");
+            echo("<th>dzial</th>");
+            echo("<th>zarobki</th>");
+            echo("<th>nazwa_dzial</th>");
+            echo("<th>data_urodzenia</th>");
+                while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                        echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("</tr>");
+                }
+            echo("</table>");
+            echo("<hr />");
+    ?>
