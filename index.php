ff9f5b92cd54924767455da6ee645ff50981237b
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-13 15:16:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php
-echo("dziala");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<strong><a href=https://github.com/SK-2019/php-sql-wprowadzenie-Olek-Zawadzki>Github</a></strong>
