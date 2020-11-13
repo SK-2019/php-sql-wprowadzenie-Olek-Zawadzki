86d78d1f4c8cff51c6cc1dc88835744a44fae096
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-13 16:31:54 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -6,7 +6,7 @@
 <body>
 <strong><a href=https://github.com/SK-2019/php-sql-wprowadzenie-Olek-Zawadzki>Github</a></strong>
 <div class="nav">
-    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="index.php">Strona Startowa</a>
     <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
     <a class="nav_link" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
     <a class="nav_link" href="data_czas.php">Data i Czas</a>
