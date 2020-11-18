8ccde33fc65896ba55a4b1f0a180b19fc4ef20b4
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-18 14:43:10 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -6,4 +6,16 @@ echo($_POST["imie"]);
 echo("<br>");
 echo($_POST["nazwisko"]);
 
+
+   require_once('conn.php');
+  $sql = "INSERT INTO pracownicy (imie, nazwisko)
+  VALUES ('John', 'Doe')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+
 ?>
