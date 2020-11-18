c72ce5d0e138193be1ed232357c41eaf843433cd
Tomjand <domagala.tomasz@gmail.com>
2020-11-18 14:51:27 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -6,6 +6,14 @@ echo($_POST["imie"]);
 echo("<br>");
 echo($_POST["nazwisko"]);
 
+$sql = "";
 
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 
 ?>
