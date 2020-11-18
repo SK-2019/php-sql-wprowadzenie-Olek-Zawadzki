03cedc4934364464052bda55dd6a41cd6ee13bed
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-18 14:47:39 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -7,8 +7,5 @@ echo("<br>");
 echo($_POST["nazwisko"]);
 
 
-require("connect.php");
-$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`) VALUES (NULL,'".$_POST['imie']."')";
-$conn->query($sql);
 
 ?>
