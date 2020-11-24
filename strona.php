cfc2e663d0635057c10d978adbe3205b3c900d4b
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-24 09:37:33 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,10 +1,11 @@
 <?php
 
-echo("jesteś na stronie.php");
-echo("<br>");
-echo($_POST["imie"]);
-echo("<br>");
-echo($_POST["nazwisko"]);
+echo("<li>Imie: ".$_POST['imie']."</li>");
+echo("<li>Dzial: ".$_POST['dzial']."</li>");
+echo("<li>Zarobki: ".$_POST['zarobki']."</li>");
+echo("<li>Data urodzenia: ".$_POST['data_ur']."</li>");
+
+  require_once("connect.php");
 
 $sql = "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,"ania",2,60,"1985-12-11")";
 
