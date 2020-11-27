1cea59a09534f854ae8c46374ee332bb75557150
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-27 13:01:11 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,6 +2,7 @@
 require_once("conn.php");
 $sql =  "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
 echo($sql);
+echo($_POST['id']);
 mysqli_query($conn,$sql);
 mysqli_close($conn);
 //header("location:https://zawadzki-olek.herokuapp.com/danedobazy.php");
