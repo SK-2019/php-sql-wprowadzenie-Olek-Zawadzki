92973736ad2642f396c438818dc780fcc41e9138
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2020-11-27 08:25:10 +0100

    Update and rename danedobazy.html to danedobazy.php

--- danedobazy.php
+++ danedobazy.php
@@ -7,6 +7,7 @@
   <a href=https://github.com/SK-2019/php-sql-wprowadzenie-Olek-Zawadzki>Github</a>
 </head>
 <body>
+      <h1>Dodawanie pracownika:</h1>
   
    <form action="insert.php" method="POST">
              <input type="text" name="imie" placeholder="imie"></br>
@@ -23,3 +24,23 @@
    <input type="submit" value="Usuń">
   </body>
 </html>
+
+
+<?php
+require_once("conn.php");
+$result=$conn->query("Select * from pracownicy");
+echo("<table border=1>");
+    echo("<th>Id</th>");
+    echo("<th>Imie</th>");
+    echo("<th>Dzial</th>");
+    echo("<th>Zarobki</th>");
+    echo("<th>Data_urodzenia</th>");
+        while($row=$result->fetch_assoc()){
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>");
+            echo("<input type='hidden' name='id' value=".$row['id_pracownicy'].">");
+            echo("<input type=submit value=X>");
+            echo("</form></td>");
+            echo("</tr>");}
+echo("</table>");
+?>
