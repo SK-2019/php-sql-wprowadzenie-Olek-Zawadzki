20e098dcabd4ae00fe62178a1dec23fba26b0fd5
Olek Zawadzki <alo.zawadzki@gmail.com>
2021-02-02 14:19:06 +0100

    .

--- pracownicy/strona.php
+++ pracownicy/strona.php
@@ -5,7 +5,7 @@ echo("<li>Dzial: ".$_POST['dzial']."</li>");
 echo("<li>Zarobki: ".$_POST['zarobki']."</li>");
 echo("<li>Data urodzenia: ".$_POST['data_ur']."</li>");
 
-  require_once("conn.php");
+  require_once("../assets/conn.php");
 
    $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_ur']."')";
    if ($conn->query($sql) === TRUE) {
